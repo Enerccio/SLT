7e9dee96dad6b70e188cab2b31149967b68c6a65
Peter Vanusanik <pvan@bach.cz>
2023-01-21 09:09:54 +0100

    fixed actions naming

--- src/main/java/com/en_circle/slt/plugin/actions/EvalFileFromEditor.java
+++ src/main/java/com/en_circle/slt/plugin/actions/EvalFileFromEditor.java
@@ -1,6 +1,5 @@
 package com.en_circle.slt.plugin.actions;
 
-import com.en_circle.slt.plugin.SltBundle;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.ex.EditorEx;
@@ -12,13 +11,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Objects;
 
 public class EvalFileFromEditor extends EvalActionBase {
-
-    @Override
-    public void update(@NotNull AnActionEvent event) {
-        super.update(event);
-
-        event.getPresentation().setText(SltBundle.message("slt.actions.eval.file"));
-    }
 
     @Override
     public void actionPerformed(@NotNull AnActionEvent event) {
--- src/main/java/com/en_circle/slt/plugin/actions/EvalPreviousFormAction.java
+++ src/main/java/com/en_circle/slt/plugin/actions/EvalPreviousFormAction.java
@@ -1,23 +1,13 @@
 package com.en_circle.slt.plugin.actions;
 
-import com.en_circle.slt.plugin.SltBundle;
 import com.en_circle.slt.plugin.lisp.psi.LispTypes;
 import com.intellij.lang.ASTNode;
-import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.editor.Document;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiWhiteSpace;
-import org.jetbrains.annotations.NotNull;
 
 public class EvalPreviousFormAction extends EvalFormActionBase {
-
-    @Override
-    public void update(@NotNull AnActionEvent event) {
-        super.update(event);
-
-        event.getPresentation().setText(SltBundle.message("slt.actions.eval.previous"));
-    }
 
     private PsiElement findElementAt(PsiFile psiFile, int offset) {
         for (int ix=offset-1; ix>=0; ix--) {
