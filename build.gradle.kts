ff69070d63c40e39a0c8da8413668f8d793b46cc
Peter Vanusanik <pvan@bach.cz>
2023-02-12 12:32:25 +0100

    minor fix so .slt stays within current ide config instead of sharing

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "com.en_circle.slt"
-version = "0.4.0"
+version = "0.4.1"
 
 repositories {
     mavenCentral()
--- src/main/java/com/en_circle/slt/tools/PluginPath.java
+++ src/main/java/com/en_circle/slt/tools/PluginPath.java
@@ -13,10 +13,6 @@ public class PluginPath {
     public static File getPluginFolder() {
         String path = PathManager.getConfigPath();
         File configPath = new File(path);
-        if (configPath.exists()) {
-            // Root jetbrains config folder
-            configPath = configPath.getParentFile();
-        }
         File pluginFolder;
         if (configPath.exists()) {
             pluginFolder = new File(configPath, PLUGIN_SUFFIX);
